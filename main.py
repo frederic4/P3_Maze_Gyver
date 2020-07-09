ada3895e44be094b0e83036c750939f571c0d815
frederic4 <frederic.guillaumin5@orange.fr>
2020-07-09 23:24:54 +0200

    affiché fenêtre pygame

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ window = pygame.display.set_mode((cote_window, cote_window))
 
 #Loading and pasting of the background
 fond = pygame.image.load("images/background.jpg").convert()
+
+continuer = 1
+while continuer:
+
+	for event in pygame.event.get(): # Waiting for events
+		pygame.display.flip()
+		if event.type == QUIT:
+			continuer = 0
